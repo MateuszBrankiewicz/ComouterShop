a42806c5c544fd613ff4a70317bb0dbb2ee62098
Mateusz Brankiewicz <bronczek522@gmail.com>
2024-03-23 21:31:44 +0100

    add product-navbar component

--- frontend/ComputerShop/src/app/home/home.component.ts
+++ frontend/ComputerShop/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SliderComponent } from '../slider/slider.component';
+import { ProductNavbarComponent } from '../product-navbar/product-navbar.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,SliderComponent],
+  imports: [HeaderComponent,SliderComponent,ProductNavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
