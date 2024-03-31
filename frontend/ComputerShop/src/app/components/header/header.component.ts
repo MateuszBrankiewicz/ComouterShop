cd5e89d991a34c5faa00b6190c378fc7593561d8
Mateusz Brankiewicz <bronczek522@gmail.com>
2024-03-31 22:54:39 +0200

    add toggle button theme style

--- frontend/ComputerShop/src/app/components/header/header.component.ts
+++ frontend/ComputerShop/src/app/components/header/header.component.ts
@@ -17,10 +17,24 @@ export class HeaderComponent  {
 
   toggleTheme() {
     this.themeService.toggleTheme();
+    this.changeButtonContent();
   }
 
   getTheme() {
     return this.themeService.getCurrentTheme();
+    this.changeButtonContent();
   }
-
+  changeButtonContent(){
+    let btnText = document.getElementById("btn-text");
+    let btnIcon = document.getElementById("btn-icon") as HTMLImageElement;
+    if(btnIcon && btnText){
+    if(this.themeService.getCurrentTheme() === "dark-theme"){
+      btnIcon.src = "../../../assets/img/theme/sun.png"
+      btnText.innerHTML = "Light"
+    }
+  else{
+    btnIcon.src = "../../../assets/img/theme/moon.png"
+    btnText.innerHTML = "Dark"
+  }}
+  }
 }
